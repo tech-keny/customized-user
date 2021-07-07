9f318f72f695b5c4c7945506e4a638a8ab3ee939
tech-keny <chanken7@icloud.com>
2021-07-07 19:51:12 +0900

    更新（ステップ４）７月７日

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "app/index.html"
+    login_url = '/accounts/login/'
+
+
