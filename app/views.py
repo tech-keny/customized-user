6fd8c56d75bc020afd04303a549a4c9ab68ce96e
tech-keny <chanken7@icloud.com>
2021-07-08 17:31:13 +0900

    書籍検索

--- app/views.py
+++ app/views.py
@@ -8,9 +8,103 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+from django.views.generic import View
+from django.shortcuts import render
+import json
+import requests
+from .forms import SearchForm
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = "app/index.html"
-    login_url = '/accounts/login/'
+
+SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=1089569001953412266' # ID変更
 
 
+def get_api_data(params):
+    api = requests.get(SEARCH_URL, params=params).text
+    result = json.loads(api)
+    items = result['Items']
+    return items
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
+
+        return render(request, 'app/index.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
+
+        if form.is_valid():
+            keyword = form.cleaned_data['title']
+            params = {
+                'title': keyword,
+                'hits': 28,
+            }
+            items = get_api_data(params)
+            book_data = []
+            for i in items:
+                item = i['Item']
+                title = item['title']
+                image = item['largeImageUrl']
+                isbn = item['isbn']
+                query = {
+                    'title': title,
+                    'image': image,
+                    'isbn': isbn
+                }
+                book_data.append(query)
+
+            return render(request, 'app/book.html', {
+                'book_data': book_data,
+                'keyword': keyword
+            })
+
+        return render(request, 'app/index.html', {
+            'form': 'form'
+        })
+
+
+class DetailView(View):
+    def get(self, request, *args, **kwargs):
+        isbn = self.kwargs['isbn']
+        params = {
+            'isbn': isbn
+        }
+
+        items = get_api_data(params)
+        items = items[0]
+        item = items['Item']
+        title = item['title']
+        image = item['largeImageUrl']
+        author = item['author']
+        itemPrice = item['itemPrice']
+        salesDate = item['salesDate']
+        publisherName = item['publisherName']
+        size = item['size']
+        isbn = item['isbn']
+        itemCaption = item['itemCaption']
+        itemUrl = item['itemUrl']
+        reviewAverage = item['reviewAverage']
+        reviewCount = item['reviewCount']
+
+        book_data = {
+            'title': title,
+            'image': image,
+            'author': author,
+            'itemPrice': itemPrice,
+            'salesDate': salesDate,
+            'publisherName': publisherName,
+            'size': size,
+            'isbn': isbn,
+            'itemCaption': itemCaption,
+            'itemUrl': itemUrl,
+            'reviewAverage': reviewAverage,
+            'reviewCount': reviewCount,
+            'average': float(reviewAverage) * 20,
+        }
+
+        return render(request, 'app/detail.html', {
+            'book_data': book_data
+        })
